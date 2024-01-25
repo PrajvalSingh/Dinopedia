390623f6c04b974e82d4b7be12c2f2515637ca2c
PrajvalSingh <prajval.laptop2323@gmail.com>
2024-01-25 19:09:32 +0530

    removed unnecessary code

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,10 +9,7 @@ if (!(typeof window)) {
 export default function Navbar() {
     return (
         <>
-            <nav className={
-                title === "dinohub.netlify.app/facts" || title === "localhost:3000/facts" ? "" : "sticky"
-                    + "top-0 text-gray-400 bg-gray-900 body-font my-2"
-            }>
+            <nav className="top-0 text-gray-400 bg-gray-900 body-font my-2">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
                         <Link href="/" className="mr-9 hover:text-white">Creatures</Link>
